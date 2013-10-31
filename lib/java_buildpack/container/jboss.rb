58cf1ec2f31253651b61561d7eeba142b5614493
Colin Humphreys <colin@hatofmonkeys.com>
2013-10-31 15:23:40 +0000

    Initial commit for JBoss build pack

---
+++ lib/java_buildpack/container/jboss.rb
@@ -0,0 +1,88 @@
+# Encoding: utf-8
+# Cloud Foundry Java Buildpack
+# Copyright (c) 2013 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'java_buildpack/container'
+require 'java_buildpack/container/container_utils'
+require 'java_buildpack/util/format_duration'
+require 'java_buildpack/util/groovy_utils'
+require 'java_buildpack/versioned_dependency_component'
+require 'tmpdir'
+
+module JavaBuildpack::Container
+
+  # Encapsulates the detect, compile, and release functionality for applications running Spring Boot CLI
+  # applications.
+  class Jboss < JavaBuildpack::VersionedDependencyComponent
+
+    def initialize(context)
+      super('JBoss', context)
+    end
+
+    def compile
+      download { |file| expand file }
+      parameterise_http_port
+    end
+
+    def release
+      java_home_string = "JAVA_HOME=#{@java_home}"
+#     java_opts_string = ContainerUtils.space("JAVA_OPTS=\"#{ContainerUtils.to_java_opts_s(@java_opts)}\"")
+
+      "#{java_home_string} #{JBOSS_HOME}/bin/standalone.sh -Dhttp.port=$PORT"
+    end
+
+    protected
+
+    def supports?
+      web_inf? && !JavaBuildpack::Util::JavaMainUtils.main_class(@app_dir)
+    end
+
+    private
+
+    WEB_INF_DIRECTORY = 'WEB-INF'.freeze
+
+    JBOSS_HOME = '.jboss'.freeze
+
+    def web_inf?
+      @application.child(WEB_INF_DIRECTORY).exist?
+    end
+
+    def expand(file)
+      expand_start_time = Time.now
+      print "       Expanding #{@component_name} to #{JBOSS_HOME} "
+
+      Dir.mktmpdir do |tmpdir_root|
+        shell "rm -rf #{jboss_home}"
+        shell "mkdir -p #{jboss_home}"
+        shell "tar xzf #{file.path} -C #{jboss_home} --strip 1 2>&1"
+      end
+
+      puts "(#{(Time.now - expand_start_time).duration})"
+    end
+
+    def jboss_home
+      File.join @app_dir, JBOSS_HOME
+    end
+
+    def parameterise_http_port
+      standalone_config = "#{jboss_home}/standalone/configuration/standalone.xml"
+      original = File.open(standalone_config, 'r') { |f| f.read }
+      modified = original.gsub(/<socket-binding name="http" port="8080"\/>/,'<socket-binding name="http" port="${http.port}"/>')
+      File.open(standalone_config, 'w') { |f| f.write modified}
+    end
+
+  end
+
+end
---
+++ spec/java_buildpack/container/jboss_spec.rb
@@ -0,0 +1,108 @@
+require 'spec_helper'
+require 'java_buildpack/container/jboss'
+
+module JavaBuildpack::Container
+
+  describe Jboss do
+
+    JBOSS_VERSION = JavaBuildpack::Util::TokenizedVersion.new('7.1.1')
+
+    JBOSS_DETAILS = [JBOSS_VERSION, 'test-jboss-uri']
+
+    let(:application_cache) { double('ApplicationCache') }
+
+    before do
+      $stdout = StringIO.new
+      $stderr = StringIO.new
+    end
+
+    it 'should detect WEB-INF' do
+      JavaBuildpack::Repository::ConfiguredItem.stub(:find_item) { |&block| block.call(JBOSS_VERSION) if block }
+      .and_return(JBOSS_DETAILS)
+      detected = Jboss.new(
+          app_dir: 'spec/fixtures/container_tomcat',
+          application: JavaBuildpack::Application.new('spec/fixtures/container_tomcat'),
+          configuration: {}
+      ).detect
+
+      expect(detected).to include('jboss=7.1.1')
+    end
+
+    it 'should not detect when WEB-INF is absent' do
+      detected = Jboss.new(
+          app_dir: 'spec/fixtures/container_main',
+          application: JavaBuildpack::Application.new('spec/fixtures/container_main'),
+          configuration: {}
+      ).detect
+
+      expect(detected).to be_nil
+    end
+
+    it 'should extract JBoss from a GZipped TAR' do
+      Dir.mktmpdir do |root|
+        Dir.mkdir File.join(root, 'WEB-INF')
+
+        JavaBuildpack::Repository::ConfiguredItem.stub(:find_item) { |&block| block.call(JBOSS_VERSION) if block }
+        .and_return(JBOSS_DETAILS)
+
+        JavaBuildpack::Util::ApplicationCache.stub(:new).and_return(application_cache)
+        application_cache.stub(:get).with('test-jboss-uri').and_yield(File.open('spec/fixtures/stub-jboss.tar.gz'))
+
+        Jboss.new(
+            app_dir: root,
+            application: JavaBuildpack::Application.new(root),
+            configuration: {}
+        ).compile
+
+        jboss_dir = File.join root, '.jboss'
+
+        standalone = File.join jboss_dir, 'bin', 'standalone.sh'
+        expect(File.exists?(standalone)).to be_true
+
+      end
+    end
+
+    it 'should set the HTTP port in the standalone configuration' do
+      Dir.mktmpdir do |root|
+        Dir.mkdir File.join(root, 'WEB-INF')
+
+        JavaBuildpack::Repository::ConfiguredItem.stub(:find_item) { |&block| block.call(JBOSS_VERSION) if block }
+        .and_return(JBOSS_DETAILS)
+
+        JavaBuildpack::Util::ApplicationCache.stub(:new).and_return(application_cache)
+        application_cache.stub(:get).with('test-jboss-uri').and_yield(File.open('spec/fixtures/stub-jboss.tar.gz'))
+
+        Jboss.new(
+            app_dir: root,
+            application: JavaBuildpack::Application.new(root),
+            configuration: {}
+        ).compile
+
+        jboss_dir = File.join root, '.jboss'
+
+        standalone = File.join jboss_dir, 'standalone', 'configuration', 'standalone.xml'
+        expect(File.exists?(standalone)).to be_true
+        File.open(standalone, 'r') do |f|
+          expect(f.read).to include('<socket-binding name="http" port="${http.port}"/>')
+        end
+
+      end
+    end
+
+    it 'should return command' do
+      JavaBuildpack::Repository::ConfiguredItem.stub(:find_item) { |&block| block.call(JBOSS_VERSION) if block }
+      .and_return(JBOSS_DETAILS)
+
+      command = Jboss.new(
+          app_dir: 'spec/fixtures/container_tomcat',
+          application: JavaBuildpack::Application.new('spec/fixtures/container_tomcat'),
+          java_home: 'test-java-home',
+          java_opts: %w(test-opt-2 test-opt-1),
+          configuration: {}
+      ).release
+
+      expect(command).to eq('JAVA_HOME=test-java-home .jboss/bin/standalone.sh -Dhttp.port=$PORT')
+    end
+
+  end
+end
