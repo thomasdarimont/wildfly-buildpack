d2a6e86b1aa85770e029cfaafe2ed38cb994daa8
Ben Hale <bhale@gopivotal.com>
2013-12-24 10:21:19 +0000

    Upgrade to Java Buildpack 2
    This change brings this fork of the Java Buildpack up to date with the
    forthcoming 2.0 version.  The new version of the buildpack brings a
    number of updates to the core of the buildpack that dramatically
    reduce the amount of code that a component must include.  In the case
    of this buildpack, both the component and its spec were reduced in
    length by around 50%.
    
    The main changes to the component were around the widespread use of
    Pathname and the use of the Application and Droplet abstractions.  The
    main changes to the spec were to use the component_helper which
    handles the setup of the test environment, reducing the need to
    explicitly do this in each spec.

--- lib/java_buildpack/container/jboss.rb
+++ lib/java_buildpack/container/jboss.rb
@@ -14,107 +14,73 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'fileutils'
+require 'java_buildpack/component/versioned_dependency_component'
 require 'java_buildpack/container'
-require 'java_buildpack/container/container_utils'
-require 'java_buildpack/util/format_duration'
 require 'java_buildpack/util/java_main_utils'
-require 'java_buildpack/versioned_dependency_component'
-require 'tmpdir'
 
 module JavaBuildpack::Container
 
   # Encapsulates the detect, compile, and release functionality for applications running Spring Boot CLI
   # applications.
-  class Jboss < JavaBuildpack::VersionedDependencyComponent
-
-    def initialize(context)
-      super('JBoss', context)
-    end
+  class Jboss < JavaBuildpack::Component::VersionedDependencyComponent
 
     def compile
-      download { |file| expand file }
-      parameterise_http_port
-      disable_welcome_root
-      link_application
+      download_tar
+      update_configuration
+      copy_application
       create_dodeploy
     end
 
     def release
-      @java_opts << '-Dhttp.port=$PORT'
+      @droplet.java_opts.add_system_property 'http.port', '$PORT'
 
-      java_home_string = "JAVA_HOME=#{@java_home}"
-      java_opts_string = ContainerUtils.space("JAVA_OPTS=\"#{ContainerUtils.to_java_opts_s(@java_opts)}\"")
-
-      "#{java_home_string}#{java_opts_string} #{JBOSS_HOME}/bin/standalone.sh -b 0.0.0.0"
+      [
+          @droplet.java_home.as_env_var,
+          @droplet.java_opts.as_env_var,
+          "$PWD/#{(@droplet.sandbox + 'bin/standalone.sh').relative_path_from(@droplet.root)}",
+          '-b',
+          '0.0.0.0'
+      ].compact.join(' ')
     end
 
     protected
 
     def supports?
-      web_inf? && !JavaBuildpack::Util::JavaMainUtils.main_class(@app_dir)
+      web_inf? && !JavaBuildpack::Util::JavaMainUtils.main_class(@application)
     end
 
     private
 
-    WEB_INF_DIRECTORY = 'WEB-INF'.freeze
-
-    JBOSS_HOME = '.jboss'.freeze
-
-    def link_application
-      FileUtils.rm_rf root
+    def copy_application
       FileUtils.mkdir_p root
-      @application.children.each { |child| FileUtils.cp_r child, root }
+      @application.root.children.each { |child| FileUtils.cp_r child, root }
     end
 
-    def web_inf?
-      @application.child(WEB_INF_DIRECTORY).exist?
-    end
-
-    def expand(file)
-      expand_start_time = Time.now
-      print "       Expanding #{@component_name} to #{JBOSS_HOME} "
-
-      Dir.mktmpdir do |tmpdir_root|
-        shell "rm -rf #{jboss_home}"
-        shell "mkdir -p #{jboss_home}"
-        shell "tar xzf #{file.path} -C #{jboss_home} --strip 1 2>&1"
-      end
-
-      puts "(#{(Time.now - expand_start_time).duration})"
-    end
-
-    def jboss_home
-      File.join @app_dir, JBOSS_HOME
-    end
-
-    def parameterise_http_port
-      standalone_config = "#{jboss_home}/standalone/configuration/standalone.xml"
-      original = File.open(standalone_config, 'r') { |f| f.read }
-      modified = original.gsub(/<socket-binding name="http" port="8080"\/>/, '<socket-binding name="http" port="${http.port}"/>')
-      File.open(standalone_config, 'w') { |f| f.write modified }
-    end
-
-    def disable_welcome_root
-      standalone_config = "#{jboss_home}/standalone/configuration/standalone.xml"
-      original = File.open(standalone_config, 'r') { |f| f.read }
-      modified = original.gsub(/<virtual-server name="default-host" enable-welcome-root="true">/, '<virtual-server name="default-host" enable-welcome-root="false">')
-      File.open(standalone_config, 'w') { |f| f.write modified }
+    def create_dodeploy
+      FileUtils.touch(webapps + 'ROOT.war.dodeploy')
     end
 
     def root
       webapps + 'ROOT.war'
     end
 
-    def jboss_home
-      @application.component_directory 'jboss'
+    def update_configuration
+      standalone_config = @droplet.sandbox + 'standalone/configuration/standalone.xml'
+
+      modified = standalone_config.read
+        .gsub(/<virtual-server name="default-host" enable-welcome-root="true">/, '<virtual-server name="default-host" enable-welcome-root="false">')
+        .gsub(/<socket-binding name="http" port="8080"\/>/, '<socket-binding name="http" port="${http.port}"/>')
+
+      standalone_config.open('w') { |f| f.write modified }
     end
 
     def webapps
-      jboss_home + 'standalone' + 'deployments'
+      @droplet.sandbox + 'standalone/deployments'
     end
 
-    def create_dodeploy
-      FileUtils.touch(webapps + 'ROOT.war.dodeploy')
+    def web_inf?
+      (@application.root + 'WEB-INF').exist?
     end
 
   end
